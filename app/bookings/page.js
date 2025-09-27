bae6e6c0a4f1a41863f7ba2e3d79afd4fd2b6578
Noor Piash <noorpiash1234@gmail.com>
2025-09-28 05:29:54 +0600

    page ready

---
+++ app/bookings/page.js
@@ -0,0 +1,27 @@
+import PastBooking from "../components/user/booking/PastBooking";
+import UpcomingBooking from "../components/user/booking/UpcomingBooking";
+import ProfileInfo from "../components/user/ProfileInfo";
+
+const BookingPage = () => {
+  return (
+    <>
+      <section className="mt-[100px]">
+        <div className="container">
+          <ProfileInfo />
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+            <PastBooking />
+
+            <UpcomingBooking />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default BookingPage;
---
+++ app/hotels/[id]/page.js
@@ -0,0 +1,15 @@
+import Summary from "@/app/components/hotel/details/Summary";
+import Gallery from "@/app/components/hotel/details/Gallery";
+import Overview from "@/app/components/hotel/details/Overview";
+
+const HotelDetailpage = () => {
+  return (
+    <div>
+      <Summary />
+      <Gallery />
+      <Overview />
+    </div>
+  );
+};
+
+export default HotelDetailpage;
