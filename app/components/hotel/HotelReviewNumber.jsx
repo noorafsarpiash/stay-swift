141ed38f75570c450d07e4d8c7b3a813a1989995
Noor Piash <noorpiash1234@gmail.com>
2025-10-10 12:20:05 +0600

    checkin and checkout

--- app/components/hotel/HotelReviewNumber.jsx
+++ app/components/hotel/HotelReviewNumber.jsx
@@ -3,12 +3,12 @@ import Link from "next/link"
 
 
 const HotelReviewNumber = async ({ id }) => {
-    const reviews = await getReviewsForHotel(id)
+
     return (
         <>
             {
-                reviews?.length === 0 ? (<Link href="" className="underline">Be the first one to review</Link>) :
-                    (<Link href={`/hotel/${id}/reviews`} className="underline">{reviews.length} Reviews</Link>)
+                id?.length === 0 ? (<Link href="" className="underline">Be the first one to review</Link>) :
+                    (<Link href={`/hotel/${id}/reviews`} className="underline">{id.length} Reviews</Link>)
             }
         </>
     )
